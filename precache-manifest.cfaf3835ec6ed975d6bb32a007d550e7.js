066e11809ada9412b642a0f240f3b86f73308030
Dominik Gyecsek <dominik.gyecsek@gmail.com>
2020-05-19 19:25:47 +0100

    Updates

---
+++ precache-manifest.cfaf3835ec6ed975d6bb32a007d550e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7ddcc40d58c1c6239df3973695882e3",
+    "url": "/private-location-tracker/static/media/appstore.e7ddcc40.png"
+  },
+  {
+    "revision": "ff1b85320070add5b225",
+    "url": "/private-location-tracker/static/js/runtime~main.4051b66b.js"
+  },
+  {
+    "revision": "e39585fdd206947d1ded",
+    "url": "/private-location-tracker/static/js/main.b981d710.chunk.js"
+  },
+  {
+    "revision": "6127ddaf4da3a97e49d3",
+    "url": "/private-location-tracker/static/js/2.2e7e10c9.chunk.js"
+  },
+  {
+    "revision": "e39585fdd206947d1ded",
+    "url": "/private-location-tracker/static/css/main.157596a4.chunk.css"
+  },
+  {
+    "revision": "6127ddaf4da3a97e49d3",
+    "url": "/private-location-tracker/static/css/2.7925247e.chunk.css"
+  },
+  {
+    "revision": "deb653a0e7a22430e87a7cd6928bafff",
+    "url": "/private-location-tracker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/private-location-tracker/precache-manifest.f84406f1ec92156c74aafd844eaf9bdd.js"
+  "/private-location-tracker/precache-manifest.cfaf3835ec6ed975d6bb32a007d550e7.js"
 );
 
 workbox.clientsClaim();
